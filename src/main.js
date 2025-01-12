6e4499f9044d8d3c391ad5ee1c11210980ae9ccd
adawg2010 <jcebersole1@gmail.com>
2025-01-12 16:05:49 +0900

    Some UI updates

--- src/main.js
+++ src/main.js
@@ -62,11 +62,6 @@ function addChannel(e) {
   savePlaylist();
   renderPlaylist();
 }
-
-
-
-
-
 function savePlaylist() {
   localStorage.setItem("lofiChannels", JSON.stringify(lofiChannels));
 }
@@ -78,12 +73,14 @@ function renderPlaylist() {
     listItem.classList.add("playlist-item");
 
     listItem.innerHTML = `
-      <span class='bg-background-light dark:bg-background-dark'>
-        ${video.title || `Video ${index + 1}`}
-      </span>
-      <button class='remove-button bg-red-700 text-white' data-index="${index}">
+      <span class='text-4xl flex flex-col gap-4  border-blue-500 border-8'> <div>
+              ${video.title || `Video ${index + 1}`}    <button class='remove-button bg-red-300 hover:bg-red-700  text-black font-semibold hover:text-white p-1 border-fuchsia-900 ' data-index="${index}">
         Remove
       </button>
+      </div>
+
+      </span>
+  
     `;
 
     channelList.appendChild(listItem);
