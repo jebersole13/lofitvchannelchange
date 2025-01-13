12c11e5f76ea3205951f8149b1e3c06b3b6355d7
adawg2010 <jcebersole1@gmail.com>
2025-01-13 11:16:11 +0900

    button changes

--- src/main.js
+++ src/main.js
@@ -81,11 +81,11 @@ function renderPlaylist() {
     listItem.classList.add("playlist-item");
 
     listItem.innerHTML = `
-      <span class=' lg:text-3xl text-base flex flex-col gap-4  border-blue-500 border-8'> <div>
-         <button type='button' class='channelButton' data-index='${index}' >${video.title || `Video ${index + 1}`}  </button>        <button class='remove-button bg-red-300 hover:bg-red-700  text-black font-semibold hover:text-white p-1 border-fuchsia-900 ' data-index="${index}">
+      <span class=' lg:text-3xl text-base flex flex-col gap-4  border-blue-500 border-8'> 
+         <button type='button' class='channelButton hover:bg-blue-300 hover:dark:bg-indigo-700 ' data-index='${index}' >${video.title || `Video ${index + 1}`}  </button>        <button class='remove-button bg-red-300 hover:bg-red-700  text-black font-semibold hover:text-white  border-fuchsia-900 ' data-index="${index}">
         Remove
       </button>
-      </div>
+      
 
       </span>
   
